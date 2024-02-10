bfc7258d21db4e3be7dc5d24c74d1b08186457f4
musz13 <muszlgx@gmail.com>
2024-02-11 01:27:06 +0800

    Update xiiiousz-bashful-generate-bash-script.sh

--- xiiiousz/bashful/xiiiousz-bashful-generate-bash-script.sh
+++ xiiiousz/bashful/xiiiousz-bashful-generate-bash-script.sh
@@ -7,7 +7,7 @@ xiiiousz_bashful "~"
 # Prompt the user for a new file name
 read -p "${XIIIOUSZ_COLOUR_01}Enter the new file name (without extension): ${XIIIOUSZ_COLOUR_02}" new_file_name
 
-use_all_package=$(get_valid_yn "${XIIIOUSZ_COLOUR_01}Include all package?")
+use_all_bashful_package=$(get_valid_yn "${XIIIOUSZ_COLOUR_01}Include all BASHFUL package?")
 
 # Specify the destination for the new script
 destination_script="$USER_DEFAULT_OUTPUT_DIRECTORIES/$new_file_name.sh"
@@ -21,14 +21,14 @@ echo "# Import functions from the source script" >>"$destination_script"
 echo "source "\$XIIIOUSZ"" >>"$destination_script"
 echo "" >>"$destination_script"
 
-# Check if use_all_package is true (equals "y")
-if [ "$use_all_package" == "y" ]; then
-    # Do something if use_all_package is true
+# Check if use_all_bashful_package is true (equals "y")
+if [ "$use_all_bashful_package" == "y" ]; then
+    # Do something if use_all_bashful_package is true
     echo "xiiiousz \"bashful\"" >>"$destination_script"
     echo "xiiiousz_bashful \"~\"" >>"$destination_script"
     echo "Including all packages..."
 else
-    # Do something else if use_all_package is not true
+    # Do something else if use_all_bashful_package is not true
     echo "Not including all packages."
 fi
 
